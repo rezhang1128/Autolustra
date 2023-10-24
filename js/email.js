b4dc1305c4832636b273af2c4397410fc8c5a52e
rezhang1128 <ryenzhang1128@gmail.com>
2023-10-25 03:07:58 +1100

    edit send email

---
+++ js/email.js
@@ -0,0 +1,44 @@
+const nodemailer = require('nodemailer');
+
+module.exports = async (req, res) => {
+    if (req.method === 'POST') {
+        const { name, phoneNumber, email, location, carDetails, service, condition } = req.body;
+
+        // Construct email content
+        const messageContent = `
+            Name: ${name}
+            Phone Number: ${phoneNumber}
+            Email: ${email}
+            Location: ${location}
+            Car Make, Model and Year: ${carDetails}
+            Enquiring service: ${service}
+            Current condition: ${condition}
+        `;
+
+        let transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com', // Replace with your email provider's details
+            port: 587,
+            secure: false, 
+            auth: {
+                user: 'autolustra@gmail.com', // Replace with your email
+                pass: '1164571045Zr!' // Replace with your email password
+            }
+        });
+
+        let mailOptions = {
+            from: 'autolustra@gmail.com', 
+            to: email, 
+            subject: 'New Customer Inquiry', 
+            text: messageContent
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return res.status(500).send({ error: error.toString() });
+            }
+            res.status(200).send({ message: 'Email sent successfully!' });
+        });
+    } else {
+        res.status(405).send({ error: 'Only POST requests allowed.' });
+    }
+}
